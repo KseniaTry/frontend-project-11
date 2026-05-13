7e99e14c22c03b69a6ea9cbd001ac117a2083995
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-05-13 14:56:49 +0300

    add ignores in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,16 @@ import js from "@eslint/js";
 import globals from "globals";
 
 export default [
+  {
+    ignores: [
+      "**/dist/**",
+      "dist",
+      "node_modules/**"
+    ],
+  },
+
   js.configs.recommended,
+
   {
     languageOptions: {
       ecmaVersion: "latest",
