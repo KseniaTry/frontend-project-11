8e27e497d64a72db882c234832e259d27d293fcb
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-05-13 15:17:21 +0300

    install stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,39 @@
-import js from "@eslint/js";
-import globals from "globals";
+import js from '@eslint/js'
+import globals from 'globals'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
   {
     ignores: [
-      "**/dist/**",
-      "dist",
-      "node_modules/**",
+      '**/dist/**',
+      'dist',
+      'node_modules/**',
     ],
   },
 
   js.configs.recommended,
 
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+    jsx: false,
+    commaDangle: 'always-multiline',
+  }),
+
   {
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
     rules: {
-      "no-console": "warn",     // Предупреждать об использовании console.log
-      "prefer-promise-reject-errors": "error", // Ошибка, если Promise.reject без Error
-      "no-async-promise-executor": "error",     // Запрещает async внутри new Promise
+      'no-console': 'warn', // Предупреждать об использовании console.log
+      'prefer-promise-reject-errors': 'error', // Ошибка, если Promise.reject без Error
+      'no-async-promise-executor': 'error', // Запрещает async внутри new Promise
     },
   },
-];
+]
